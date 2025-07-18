965b49054bfd8563ce104983e7883b7e53b97029
Nice3point <nice3point@gmail.com>
2025-07-18 15:08:32 +0300

    Update packing workflow

--- build/Build.Pack.cs
+++ build/Build.Pack.cs
@@ -4,17 +4,28 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 partial class Build
 {
     Target Pack => _ => _
-        .DependsOn(Compile)
+        .DependsOn(Clean)
         .Requires(() => ReleaseVersion)
         .Executes(() =>
         {
-            foreach (var configuration in GlobBuildConfigurations())
+            var readme = CreateNugetReadme();
+            try
             {
-                DotNetPack(settings => settings
-                    .SetConfiguration(configuration)
-                    .SetVersion(GetPackVersion(configuration))
-                    .SetOutputDirectory(ArtifactsDirectory)
-                    .SetVerbosity(DotNetVerbosity.minimal));
+                var packConfigurations = GlobBuildConfigurations()
+                    .Where(configuration => configuration.Contains(" R", StringComparison.OrdinalIgnoreCase));
+
+                foreach (var configuration in packConfigurations)
+                {
+                    DotNetPack(settings => settings
+                        .SetConfiguration(configuration)
+                        .SetVersion(GetPackVersion(configuration))
+                        .SetOutputDirectory(ArtifactsDirectory)
+                        .SetVerbosity(DotNetVerbosity.minimal));
+                }
+            }
+            finally
+            {
+                RestoreReadme(readme);
             }
         });
 
@@ -22,5 +33,29 @@ partial class Build
     {
         if (PackageVersionsMap.TryGetValue(configuration, out var version)) return version;
         throw new Exception($"Can't find pack version for configuration: {configuration}");
+    }
+
+    string CreateNugetReadme()
+    {
+        var readmePath = Solution.Directory / "Readme.md";
+        var readme = File.ReadAllText(readmePath);
+
+        const string startSymbol = "<p";
+        const string endSymbol = "</p>\r\n\r\n";
+
+        var logoStartIndex = readme.IndexOf(startSymbol, StringComparison.Ordinal);
+        var logoEndIndex = readme.IndexOf(endSymbol, StringComparison.Ordinal);
+
+        var nugetReadme = readme.Remove(logoStartIndex, logoEndIndex - logoStartIndex + endSymbol.Length);
+        File.WriteAllText(readmePath, nugetReadme);
+
+        return readme;
+    }
+
+    void RestoreReadme(string readme)
+    {
+        var readmePath = Solution.Directory / "Readme.md";
+
+        File.WriteAllText(readmePath, readme);
     }
 }
